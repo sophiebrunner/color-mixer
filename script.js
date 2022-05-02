72b1a0885dc2485547356652a9b9ef44cc1e214a
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-05-02 12:03:32 +0200

    feat: add functions to convert rgb-values to hex on the label

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ const blueValue = document.querySelector("#blue");
 
 const colorLabel = document.querySelector("label[for=slider-container]");
 
+let rgbToHex = function (rgb) {
+  let hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+    hex = "0" + hex;
+  }
+  return hex;
+};
+
+let fullColorHex = function (r, g, b) {
+  let red = rgbToHex(r);
+  let green = rgbToHex(g);
+  let blue = rgbToHex(b);
+  return red + green + blue;
+};
+
 function changeColorLabel() {
   colorLabel.innerText =
-    "rgb(" +
-    redValue.value +
-    "," +
-    greenValue.value +
-    "," +
-    blueValue.value +
-    ")";
+    "#" + fullColorHex(redValue.value, greenValue.value, blueValue.value);
 }
 
 redValue.addEventListener("change", function () {
