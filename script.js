2d0a1d7c9b4457c716453190d9aa731568fad6a9
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-05-20 19:45:56 +0200

    feat: add random-color-function  with fetch API

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
-const main = document.querySelector("main");
-const header = document.querySelector("header");
+function renderColorApp() {
+  const main = document.querySelector("main");
+  const header = document.querySelector("header");
 
-const redValue = document.querySelector("#red");
-const greenValue = document.querySelector("#green");
-const blueValue = document.querySelector("#blue");
+  const colorSlider = document.querySelector("#slider-container");
 
-const colorLabel = document.querySelector("label[for=slider-container]");
+  const redValue = document.querySelector("#red");
+  const greenValue = document.querySelector("#green");
+  const blueValue = document.querySelector("#blue");
 
-let rgbToHex = function (rgb) {
-  let hex = Number(rgb).toString(16);
-  if (hex.length < 2) {
-    hex = "0" + hex;
+  const colorLabel = document.querySelector("label[for=slider-container]");
+  const btnRandomColor = document.querySelector("#btn-rndm-clr");
+
+  colorSlider.addEventListener("change", function () {
+    changeColor("--red-value", redValue.value);
+    changeColor("--green-value", greenValue.value);
+    changeColor("--blue-value", blueValue.value);
+    changeColorLabel();
+  });
+
+  btnRandomColor.addEventListener("click", getRandomColor);
+
+  function changeColor(customColor, colorValue) {
+    main.style.setProperty(customColor, colorValue);
+    header.style.setProperty(customColor, colorValue);
   }
-  return hex;
-};
 
-let fullColorHex = function (r, g, b) {
-  let red = rgbToHex(r);
-  let green = rgbToHex(g);
-  let blue = rgbToHex(b);
-  return red + green + blue;
-};
+  let rgbToHex = function (rgb) {
+    let hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+      hex = "0" + hex;
+    }
+    return hex;
+  };
 
-function changeColorLabel() {
-  colorLabel.innerText =
-    "#" + fullColorHex(redValue.value, greenValue.value, blueValue.value);
+  let fullColorHex = function (r, g, b) {
+    let red = rgbToHex(r);
+    let green = rgbToHex(g);
+    let blue = rgbToHex(b);
+    return red + green + blue;
+  };
+
+  function changeColorLabel() {
+    colorLabel.innerText =
+      "#" + fullColorHex(redValue.value, greenValue.value, blueValue.value);
+  }
+
+  function getRandomColor() {
+    fetch("https://dummy-apis.netlify.app/api/color")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        changeColor("--red-value", data.rgb.r);
+        redValue.value = data.rgb.r;
+        changeColor("--green-value", data.rgb.g);
+        greenValue.value = data.rgb.g;
+        changeColor("--blue-value", data.rgb.b);
+        blueValue.value = data.rgb.b;
+        colorLabel.innerText = data.color;
+      });
+  }
 }
 
-redValue.addEventListener("change", function () {
-  main.style.setProperty("--red-value", redValue.value);
-  header.style.setProperty("--red-value", redValue.value);
-  changeColorLabel();
-});
-
-greenValue.addEventListener("change", function () {
-  main.style.setProperty("--green-value", greenValue.value);
-  header.style.setProperty("--green-value", greenValue.value);
-  changeColorLabel();
-});
-
-blueValue.addEventListener("change", function () {
-  main.style.setProperty("--blue-value", blueValue.value);
-  header.style.setProperty("--blue-value", blueValue.value);
-  changeColorLabel();
-});
+renderColorApp();
